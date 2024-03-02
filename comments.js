2f9f8403a4969e2e7820392f7c7937978ed1d949
Nard Théo <58373303+PandeoF1@users.noreply.github.com>
2024-03-02 18:45:53 +0000

    oui

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+});
